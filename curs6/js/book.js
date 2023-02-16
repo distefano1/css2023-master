bc4f86449a88aa461a8804eb606ed2ac71945558
distefano1 <117753594+distefano1@users.noreply.github.com>
2023-02-16 20:57:49 +0200

    curs

---
+++ curs6/js/book.js
@@ -0,0 +1,16 @@
+function Book(title,author,page) {
+    this.title = title;
+    this.author =author;
+    this.page = page;
+    this.toString = toString;
+}
+
+function toString() {
+    return this.title + 'de ' +
+    this.author + ',are ' +
+    this.page + ' pagini'
+}
+function citeste() {
+    return "Acum citesc o carte";
+}
+export {Book, citeste}
